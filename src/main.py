ef65457082b62df03cf901389e363ecf840fd1f1
Raj Dave <10864@bpsdoha.edu.qa>
2022-11-02 18:27:15 +0300

    add list to main.py

--- src/main.py
+++ src/main.py
@@ -28,4 +28,29 @@ class Circular:
         if json['http_status'] == 200:
             return json['data']
 
+    # /list endpoint
+    def list(self, category: str or int, amount: int = -1) -> list or None:
+        if type(category) == int:
+            category = int(category)
+            if not 1 < category < 100:
+                raise ValueError("Invalid category Number")
 
+        else:
+            if category not in ["ptm", "general", "exam"]:
+                raise ValueError("Invalid category Name")
+
+        if amount < -1:
+            amount = -1
+
+        params = {'category': category}
+
+        request = requests.get(self.url + "list", params=params)
+        json = request.json()
+        try:
+            json['http_status']
+        except KeyError:
+            raise ValueError("Invalid API Response")
+        if json['http_status'] == 200:
+            return json['data'] if amount == -1 else json['data'][:amount]
+
+    
